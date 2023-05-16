0107b4ecfc8d50db50af4a01bb7f8e59ffe1a3d7
nrennie <nrennie35@gmail.com>
2023-05-16 21:20:55 +0100

    feat: define function to create edge labels

---
+++ R/get_edge_labels.R
@@ -0,0 +1,39 @@
+#' Get edge labels
+#'
+#' Define the end points of the arrows
+#'
+#' @param data Data frame or tibble of edges. Must have at least two columns, if not labelled "from"
+#' and "to" it is assumed first column are "from" node names,
+#' second column is "to" node names. Node names must be unique.
+#' @param plot_edges Tibble of edge coordinates output from \code{get_edges()}.
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+#' @return A tibble.
+#' @noRd
+
+get_edge_labels <- function(
+    data,
+    plot_edges) {
+  if (is.null(data[["label"]])) {
+    return(NULL)
+  } else {
+    edge_labels <- plot_edges %>%
+      dplyr::group_by(.data$id) %>%
+      dplyr::mutate(
+        text_x = mean(.data$x),
+        text_y = mean(.data$y)
+      ) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(.data$s_e, .data$name, .data$text_x, .data$text_y) %>%
+      tidyr::pivot_wider(
+        names_from = .data$s_e,
+        values_from = .data$name
+      ) %>%
+      dplyr::left_join(
+        data,
+        by = c("from", "to")
+      ) %>%
+      dplyr::select(.data$text_x, .data$text_y, .data$label)
+    return(edge_labels)
+  }
+}
