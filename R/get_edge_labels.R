aca5b3b770cdd56151a7a12c1ec8934dd98b1652
nrennie <nrennie35@gmail.com>
2023-05-16 21:34:31 +0100

    chore: add checks to data

--- R/get_edge_labels.R
+++ R/get_edge_labels.R
@@ -17,6 +17,25 @@ get_edge_labels <- function(
   if (is.null(data[["label"]])) {
     return(NULL)
   } else {
+    # check number of columns
+    if (ncol(data) < 2) {
+      stop("Incorrect number of columns in data: data should contain at least two columns.")
+    }
+    # define notin
+    "%notin%" <- function(x, y) {
+      !("%in%"(x, y))
+    }
+    # check column names
+    if ("from" %in% colnames(data) && "to" %notin%  colnames(data)) {
+      stop("data contains only 'from' column but not 'to'")
+    }
+    if ("to" %in% colnames(data) && "from" %notin%  colnames(data)) {
+      stop("data contains only 'to' column but not 'from'")
+    }
+    if (!all(c("from", "to") %in% colnames(data))) {
+      colnames(data)[1:2] <- c("from", "to")
+      message("'from' and 'to' not found in column names, using first two columns instead.")
+    }
     edge_labels <- plot_edges %>%
       dplyr::group_by(.data$id) %>%
       dplyr::mutate(
