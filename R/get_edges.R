b91af57847895833a0da2f956d55add2e6b2ee78
nrennie <nrennie35@gmail.com>
2023-05-19 16:06:30 +0100

    feat: add node_layout as an argument for edges

--- R/get_edges.R
+++ R/get_edges.R
@@ -6,18 +6,26 @@
 #' and "to" it is assumed first column are "from" node names,
 #' second column is "to" node names. Node names must be unique.
 #' @param plot_nodes Tibble of node coordinates output from \code{get_nodes()}.
+#' @param node_layout Data frame of node layout returned by \code{get_layout()}
 #' @importFrom dplyr %>%
 #' @importFrom rlang .data
 #' @return A tibble.
 #' @noRd
 
-get_edges <- function(data, plot_nodes) {
+get_edges <- function(data, plot_nodes, node_layout) {
   if (ncol(data) < 2) {
     stop("Incorrect number of columns in data: input data should contain at least two columns.")
   }
   if (!all(c("from", "to") %in% colnames(data))) {
     colnames(data)[1:2] <- c("from", "to")
   }
+  # define edge types
+  edge_type_data <- arrow_direction(
+    data = data,
+    plot_nodes = plot_nodes,
+    node_layout = node_layout
+    )
+  # compute start and end points
   plot_edges <- data %>%
     dplyr::select(c(.data$from, .data$to)) %>%
     dplyr::mutate(id = dplyr::row_number()) %>%
